77bbab0d02f493b0d1f3d0a4e5989cc1515892ad
Anisha Narang <anarang@redhat.com>
2015-08-11 01:37:18 +0530

    Adding test to verify if 'Unregister system' works correctly for RHAI.

--- robottelo/ui/locators.py
+++ robottelo/ui/locators.py
@@ -1987,5 +1987,16 @@ locators = LocatorDict({
         ("//div[@class='system-summary']/p")),
     "insights.org_selection_msg": (
         By.ID, "content"),
-
+    "insights.unregister_system": (
+        By.XPATH, (
+            "//a[contains(.,'%s')]/../../td/a[contains("
+            "@title,'Unregister System')]")),
+    "insights.unregister_button": (
+        By.XPATH, (
+            "//div[@class='sweet-alert showSweetAlert visible']//"
+            "button[@class='confirm']")),
+    "insights.no_systems_element": (
+        By.XPATH, (
+            "//div[@class='text-center']//h4")
+        ),
 })
